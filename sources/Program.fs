e32a73918139fefa1c1a6cad36937e2e5a0f0a31
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2021-12-01 23:25:42 +0100

    Prepare day 2

--- sources/Program.fs
+++ sources/Program.fs
@@ -1,5 +1,9 @@
 ﻿open Year2021Day1
+open Year2021Day2
 
 printfn "Hello from F#"
 printfn "Day 1 part 1 solution is %i" SolveDay1Part1
 printfn "Day 1 part 2 solution is %i" SolveDay1Part2
+
+printfn "Day 2 part 1 solution is %i" SolveDay2Part1
+printfn "Day 2 part 2 solution is %i" SolveDay2Part2
---
+++ sources/Year2021Day2.fs
@@ -0,0 +1,12 @@
+module Year2021Day2
+
+open System.IO
+
+let inputs = 
+    File.ReadAllLines("inputs/year2021day2.txt")
+
+let SolveDay2Part1 = 
+    -1
+
+let SolveDay2Part2 = 
+    -1
