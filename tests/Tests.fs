34c479c93bf230dc1dd68ab928e1a97827316d95
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2021-12-02 09:25:12 +0100

    Add tests and include input files in the fsproj content which is copied to the build output folder

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -1,7 +1,23 @@
 module Tests
 
-open System
 open Xunit
+open FsUnit.Xunit
+
+open Year2021Day1
+open Year2021Day2
 
 [<Fact>]
-let ``My test`` () = Assert.True(true)
+let ``Day 1 Part 1: `` () = 
+    SolveDay1Part1 |> should equal 1688
+
+[<Fact>]
+let ``Day 1 Part 2: `` () = 
+    SolveDay1Part2 |> should equal 1728
+
+[<Fact>]
+let ``Day 2 Part 1: `` () = 
+    SolveDay2Part1 |> should equal 1693300
+
+[<Fact>]
+let ``Day 2 Part 2: `` () = 
+    SolveDay2Part2 |> should equal 1857958050
