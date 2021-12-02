5c56df8c4efd5342293d427da7d3fe9eb098dbe4
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2021-12-02 14:59:45 +0100

    Prepare day 3

---
+++ sources/Year2021Day3.fs
@@ -0,0 +1,13 @@
+module Year2021Day3
+
+open System.IO
+
+let inputs =
+    File.ReadAllLines("inputs/year2021day3.txt")
+
+
+let SolveDay3Part1 =
+    -1
+
+let SolveDay3Part2 =
+    -1
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -5,6 +5,7 @@ open FsUnit.Xunit
 
 open Year2021Day1
 open Year2021Day2
+open Year2021Day3
 
 [<Fact>]
 let ``Day 1 Part 1: Number of times a depth measurement increases`` () = 
@@ -21,3 +22,11 @@ let ``Day 2 Part 1: Multiply final horizontal and depth (without aim)`` () =
 [<Fact>]
 let ``Day 2 Part 2: Mutiply final horizontal and depth (using aim)`` () = 
     SolveDay2Part2 |> should equal 1857958050
+
+[<Fact>]
+let ``Day 3 Part 1:`` () = 
+    SolveDay3Part1 |> should equal 0
+
+[<Fact>]
+let ``Day 3 Part 1: `` () = 
+    SolveDay3Part2 |> should equal 0
