5766d2f21f3c561885e10299f5b821752793e5a6
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2021-12-02 14:53:48 +0100

    Add meaningfull test names

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -7,17 +7,17 @@ open Year2021Day1
 open Year2021Day2
 
 [<Fact>]
-let ``Day 1 Part 1: `` () = 
+let ``Day 1 Part 1: Number of times a depth measurement increases`` () = 
     SolveDay1Part1 |> should equal 1688
 
 [<Fact>]
-let ``Day 1 Part 2: `` () = 
+let ``Day 1 Part 2: Number of times a three-measurement sliding window increases`` () = 
     SolveDay1Part2 |> should equal 1728
 
 [<Fact>]
-let ``Day 2 Part 1: `` () = 
+let ``Day 2 Part 1: Multiply final horizontal and depth (without aim)`` () = 
     SolveDay2Part1 |> should equal 1693300
 
 [<Fact>]
-let ``Day 2 Part 2: `` () = 
+let ``Day 2 Part 2: Mutiply final horizontal and depth (using aim)`` () = 
     SolveDay2Part2 |> should equal 1857958050
