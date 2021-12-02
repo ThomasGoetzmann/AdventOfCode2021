b1be3a2569e52f0d89bfa86bc88b0aa9ae7f8f52
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2021-12-02 07:35:53 +0100

    Refactoring day 2

--- sources/Year2021Day1.fs
+++ sources/Year2021Day1.fs
@@ -2,20 +2,19 @@ module Year2021Day1
 
 open System.IO
 
-let inputs = 
+let inputs =
     File.ReadAllLines("inputs/year2021day1.txt")
     |> Seq.map int
 
-let SolveDay1Part1 = 
+let SolveDay1Part1 =
     inputs
     |> Seq.pairwise
-    |> Seq.filter (fun (x,y) -> x < y)
+    |> Seq.filter (fun (x, y) -> x < y)
     |> Seq.length
 
-let SolveDay1Part2 = 
+let SolveDay1Part2 =
     inputs
     |> Seq.windowed 3
     |> Seq.pairwise
-    |> Seq.filter (fun (x,y) -> Seq.sum x < Seq.sum y )
+    |> Seq.filter (fun (x, y) -> Seq.sum x < Seq.sum y)
     |> Seq.length
-    
--- sources/Year2021Day2.fs
+++ sources/Year2021Day2.fs
@@ -3,66 +3,65 @@ module Year2021Day2
 open System.IO
 open System.Text.RegularExpressions
 
-let inputs = 
+let inputs =
     File.ReadAllLines("inputs/year2021day2.txt")
     |> List.ofSeq
 
 type Instruction =
-    | Forward of int
     | Up of int
     | Down of int
+    | Forward of int
 
-type Position = {
-    Horizontal: int
-    Depth: int
-    Aim : int
-}
+type Position =
+    { Horizontal: int
+      Depth: int
+      Aim: int }
 
-let parse line = 
+let parse line =
     let pattern = @"(?'direction'.*) (?'number'\d*)"
     let m = Regex.Match(line, pattern)
 
     match m.Groups["direction"].Value with
-    | "forward" -> Forward(int m.Groups["number"].Value)
-    | "down" -> Down(int m.Groups["number"].Value)
-    | "up" -> Up(int m.Groups["number"].Value)
+    | "up" -> Up (int m.Groups["number"].Value)
+    | "down" -> Down (int m.Groups["number"].Value)
+    | "forward" -> Forward (int m.Groups["number"].Value)
     | _ -> failwith "Invalid input"
-    
-let Part1MovesFrom initialPosition instructions =
-    let rec Move p instructions = 
-        match instructions with
-        | [] -> p
-        | head :: tail -> 
-            match head with
-            | Up x -> Move {p with Depth = p.Depth - x} tail
-            | Down x -> Move {p with Depth = p.Depth + x} tail
-            | Forward x-> Move {p with Horizontal= p.Horizontal + x} tail
-    
-    Move initialPosition instructions
 
-let Part2MovesFrom initialPosition instructions =
-    let rec Move p instructions = 
+let Part1Rules instruction p =
+    match instruction with
+    | Up x -> { p with Depth = p.Depth - x }
+    | Down x -> { p with Depth = p.Depth + x }
+    | Forward x -> { p with Horizontal = p.Horizontal + x }
+
+let Part2Rules instruction p =
+    match instruction with
+    | Up x -> { p with Aim = p.Aim - x }
+    | Down x -> { p with Aim = p.Aim + x }
+    | Forward x ->
+        { p with
+              Horizontal = p.Horizontal + x
+              Depth = p.Depth + (p.Aim * x) }
+
+let RunWith rules instructions =
+    let rec Move position instructions =
         match instructions with
-        | [] -> p
-        | head :: tail  ->
-            match head with
-            | Up x -> Move {p with Aim= p.Aim - x} tail
-            | Down x -> Move {p with Aim= p.Aim + x} tail
-            | Forward x -> Move {p with Horizontal= p.Horizontal + x; Depth = p.Depth + (p.Aim * x)} tail
-    
+        | [] -> position
+        | head :: tail -> Move (rules head position) tail
+
+    let initialPosition = { Horizontal = 0; Depth = 0; Aim = 0 }
     Move initialPosition instructions
 
 let Multiply p = 
     p.Horizontal * p.Depth
 
-let SolveDay2Part1 = 
+let SolveDay2Part1 =
     inputs
     |> List.map parse
-    |> Part1MovesFrom {Horizontal= 0; Depth =0; Aim=0}
+    |> RunWith Part1Rules
     |> Multiply
 
-let SolveDay2Part2 = 
+let SolveDay2Part2 =
     inputs
     |> List.map parse
-    |> Part2MovesFrom { Horizontal=0; Depth=0; Aim=0}
+    |> RunWith Part2Rules
     |> Multiply
